705b6f23c9180ac5c0502a00b146ed67b217b5e9
BlueWorldInc <keamybakunin@gmail.com>
2022-04-28 01:06:22 +0200

    save

--- playground/Duree/main.cpp
+++ playground/Duree/main.cpp
@@ -2,10 +2,7 @@
 #include "Duree.h"
 
 int main() {
-	Duree d1(0, 20, 52), d2(4, 44, 15), d3(0, 5, 12);
-	// std::cout << "d1 + d2" << std::endl;
-	// d1 += 55;
-	// d1.affiche();
+	Duree d1(0, 20, 53), d2(4, 44, 57), d3(0, 5, 12);
 	std::cout << d1 + d2 << std::endl;
 	return 0;
 }
