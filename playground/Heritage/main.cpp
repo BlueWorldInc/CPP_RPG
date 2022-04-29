309022fd6dbc89d365b3bbdb6faa04c1609c49f6
BlueWorldInc <keamybakunin@gmail.com>
2022-04-29 18:28:26 +0200

    save

--- playground/Heritage/main.cpp
+++ playground/Heritage/main.cpp
@@ -10,21 +10,48 @@ int main() {
 	Guerrier goliath("Goliath");
 	Magicien merlin("Merlin");
 	MagicienBlanc mera("Mera");
-	MagicienNoir camor("Camor", 15);
+	MagicienNoir costa("Costa", 15);
+	Guerrier miki("Miki");
 
 	alfred.donnerCoupDePoingt(goliath);
 	merlin.bouleDeGlace(goliath);
 	goliath.attaquePuissanteAuMarteau(merlin);
 	merlin.donnerCoupDePoingt(alfred);
 
-	alfred.getStatus();
-	goliath.getStatus();
-	merlin.getStatus();
-	mera.soigner(merlin);
-	merlin.getStatus();
-	camor.mortifier(merlin);
-	merlin.getStatus();
-	std::cout << camor.estVivant();
+	// alfred.getStatus();
+	// goliath.getStatus();
+	// merlin.getStatus();
+	// mera.soigner(merlin);
+	// merlin.getStatus();
+	// costa.mortifier(merlin);
+	// merlin.getStatus();
+
+	std::cout << "Choisir une cible: 1: Alfred, 2: Goliath, 3: Merlin, 4: Mera, 5: Costa";
+	int choixUtilisateur = 0;
+	std::cin >> choixUtilisateur;
+
+	Personnage cible;
+
+	switch (choixUtilisateur) {
+	case 1:
+		cible = alfred;
+		break;
+	case 2:
+		cible = goliath;
+		break;
+	case 3:
+		cible = merlin;
+		break;
+	case 4:
+		cible = mera;
+		break;
+	case 5:
+		cible = costa;
+		break;
+	}
+
+	miki.donnerCoupDePoingt(cible);
+	cible.getStatus();
 
 	return 0;
 }
